c02bdb23b4402ef6b464e5d70e269b90648d1d40
Eric Nicolas (ccjmne) <ccjmne@gmail.com>
2023-08-10 12:33:48 +0200

    Align UserScript version to npm version

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const webpack = require('webpack')
 
+const { version } = require('./package.json')
+
 // The built-in webpack.BannerPlugin somehow doesn't output the banner in production mode.
 // See https://github.com/webpack/webpack/issues/6630
 // Credit: https://github.com/webpack/webpack-cli/issues/312#issuecomment-732749280
@@ -55,7 +57,7 @@ module.exports = {
       // ==UserScript==
       // @name         Replace text in Laufzettels
       // @namespace    http://tampermonkey.net/
-      // @version      1.0
+      // @version      ${version}
       // @author       Eric NICOLAS (ccjmne) <ccjmne@gmail.com>
       // @match        https://backoffice.intern.mercateo.com/prototype.tgui.server/
       // @icon         https://www.google.com/s2/favicons?sz=64&domain=mercateo.com
